7415e150fbd2afdbd5ac0acfd6bbf6f1f654cf3a
OscarLahaie <oscarlahaie83@gmail.com>
2017-01-30 18:33:28 +0100

    Add files via upload

---
+++ Reon-AI.py
@@ -0,0 +1,12 @@
+print('Bonjour')
+wait input('t')
+name = input ("quelle est ton nom?")
+age = input ("et quelle age?")
+if name == Oscar and age == 13:
+password = (FarSonic05)
+Verification = 0
+while 1==Verifcation:
+if password == input "Mot de passe?":
+Verification + 1
+Else:
+Print ("C'est faux")
